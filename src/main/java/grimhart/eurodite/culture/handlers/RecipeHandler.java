1ac485a8e26f8451b41567e958832c2a349a8c55
The_Fireplace <the.f1repl4ce@gmail.com>
2016-11-20 18:34:19 -0600

    Use OreDict

--- src/main/java/grimhart/eurodite/culture/handlers/RecipeHandler.java
+++ src/main/java/grimhart/eurodite/culture/handlers/RecipeHandler.java
@@ -6,64 +6,67 @@ import grimhart.eurodite.culture.init.ModTools;
 import net.minecraft.init.Items;
 import net.minecraft.item.ItemStack;
 import net.minecraftforge.fml.common.registry.GameRegistry;
+import net.minecraftforge.oredict.ShapedOreRecipe;
+import net.minecraftforge.oredict.ShapelessOreRecipe;
 
 public class RecipeHandler {
 
 	 public static void registerCraftingRecipes() {
-		 GameRegistry.addRecipe(new ItemStack(ModItems.tinBag, 1), "CCC", "CCC", "CCC", 'I', Items.IRON_INGOT, 'S', Items.STICK, 'C', ModItems.tinIngot);
-		 GameRegistry.addRecipe(new ItemStack(ModTools.longSword, 1), " I ", " I ", "CS ", 'I', Items.IRON_INGOT, 'S', Items.STICK, 'C', ModItems.tinBag);
+		 addRecipe(new ItemStack(ModItems.tinBag, 1), "CCC", "CCC", "CCC", 'I', "ingotIron", 'S', "stickWood", 'C', ModItems.tinIngot);
+		 addRecipe(new ItemStack(ModTools.longSword, 1), " I ", " I ", "CS ", 'I', "ingotIron", 'S', "stickWood", 'C', ModItems.tinBag);
 		 
 		 //RED
-		 GameRegistry.addShapelessRecipe(new ItemStack(ModArmour.EuroditeHelmet, 1), Items.IRON_HELMET, ModItems.tinBag, Items.POTATO, Items.POTATO);
-		 GameRegistry.addShapelessRecipe(new ItemStack(ModArmour.EuroditeChestplate, 1), Items.IRON_CHESTPLATE, ModItems.tinBag, Items.POTATO, Items.POTATO);
-		 GameRegistry.addShapelessRecipe(new ItemStack(ModArmour.EuroditeLeggings, 1), Items.IRON_LEGGINGS, ModItems.tinBag, new ItemStack(Items.DYE, 1, 1));
-		 GameRegistry.addShapelessRecipe(new ItemStack(ModArmour.EuroditeBoots, 1), Items.IRON_BOOTS, ModItems.tinBag, Items.POTATO, Items.POTATO);
+		 addShapelessRecipe(new ItemStack(ModArmour.EuroditeHelmet, 1), Items.IRON_HELMET, ModItems.tinBag, Items.POTATO, Items.POTATO);
+		 addShapelessRecipe(new ItemStack(ModArmour.EuroditeChestplate, 1), Items.IRON_CHESTPLATE, ModItems.tinBag, Items.POTATO, Items.POTATO);
+		 addShapelessRecipe(new ItemStack(ModArmour.EuroditeLeggings, 1), Items.IRON_LEGGINGS, ModItems.tinBag, "dyeRed");
+		 addShapelessRecipe(new ItemStack(ModArmour.EuroditeBoots, 1), Items.IRON_BOOTS, ModItems.tinBag, Items.POTATO, Items.POTATO);
 		 
-		 GameRegistry.addShapelessRecipe(new ItemStack(ModArmour.EuroditeHelmetB, 1), Items.IRON_HELMET, ModItems.tinBag);
-		 GameRegistry.addShapelessRecipe(new ItemStack(ModArmour.EuroditeChestplateB, 1), Items.IRON_CHESTPLATE, ModItems.tinBag);
-		 GameRegistry.addShapelessRecipe(new ItemStack(ModArmour.EuroditeLeggingsB, 1), Items.IRON_LEGGINGS, ModItems.tinBag, new ItemStack(Items.DYE, 1, 1), new ItemStack(Items.DYE, 1, 1));
+		 addShapelessRecipe(new ItemStack(ModArmour.EuroditeHelmetB, 1), Items.IRON_HELMET, ModItems.tinBag);
+		 addShapelessRecipe(new ItemStack(ModArmour.EuroditeChestplateB, 1), Items.IRON_CHESTPLATE, ModItems.tinBag);
+		 addShapelessRecipe(new ItemStack(ModArmour.EuroditeLeggingsB, 1), Items.IRON_LEGGINGS, ModItems.tinBag, "dyeRed", "dyeRed");
 	
-		 GameRegistry.addShapelessRecipe(new ItemStack(ModArmour.EuroditeHelmetC, 1), Items.IRON_HELMET, ModItems.tinBag, Items.POTATO);
-		 GameRegistry.addShapelessRecipe(new ItemStack(ModArmour.EuroditeChestplateC, 1), Items.IRON_CHESTPLATE, ModItems.tinBag, Items.POTATO);
-		 GameRegistry.addShapelessRecipe(new ItemStack(ModArmour.EuroditeLeggingsC, 1), Items.IRON_LEGGINGS, ModItems.tinBag, new ItemStack(Items.DYE, 1, 1), new ItemStack(Items.DYE, 1, 1), new ItemStack(Items.DYE, 1, 1));
+		 addShapelessRecipe(new ItemStack(ModArmour.EuroditeHelmetC, 1), Items.IRON_HELMET, ModItems.tinBag, Items.POTATO);
+		 addShapelessRecipe(new ItemStack(ModArmour.EuroditeChestplateC, 1), Items.IRON_CHESTPLATE, ModItems.tinBag, Items.POTATO);
+		 addShapelessRecipe(new ItemStack(ModArmour.EuroditeLeggingsC, 1), Items.IRON_LEGGINGS, ModItems.tinBag, "dyeRed", "dyeRed", "dyeRed");
 		 
 		 
 		 
 		 //Blue
-		 GameRegistry.addShapelessRecipe(new ItemStack(ModArmour.EuroditeHelmetD, 1), Items.IRON_HELMET, ModItems.tinBag, Items.POTATO, Items.POTATO, Items.POTATO, Items.POTATO);
-		 GameRegistry.addShapelessRecipe(new ItemStack(ModArmour.EuroditeChestplateD, 1), Items.IRON_CHESTPLATE, ModItems.tinBag, Items.POTATO, Items.POTATO, Items.POTATO, Items.POTATO);
-		 GameRegistry.addShapelessRecipe(new ItemStack(ModArmour.EuroditeLeggingsD, 1), Items.IRON_LEGGINGS, ModItems.tinBag, new ItemStack(Items.DYE, 1, 4));
+		 addShapelessRecipe(new ItemStack(ModArmour.EuroditeHelmetD), Items.IRON_HELMET, ModItems.tinBag, Items.POTATO, Items.POTATO, Items.POTATO, Items.POTATO);
+		 addShapelessRecipe(new ItemStack(ModArmour.EuroditeChestplateD), Items.IRON_CHESTPLATE, ModItems.tinBag, Items.POTATO, Items.POTATO, Items.POTATO, Items.POTATO);
+		 addShapelessRecipe(new ItemStack(ModArmour.EuroditeLeggingsD), Items.IRON_LEGGINGS, ModItems.tinBag, "dyeBlue");
 		 //
-		 GameRegistry.addShapelessRecipe(new ItemStack(ModArmour.EuroditeHelmetE, 1), Items.IRON_HELMET, ModItems.tinBag, Items.POTATO, Items.POTATO, Items.POTATO, Items.POTATO, Items.POTATO);
-		 GameRegistry.addShapelessRecipe(new ItemStack(ModArmour.EuroditeChestplateE, 1), Items.IRON_CHESTPLATE, ModItems.tinBag, Items.POTATO);
-		 GameRegistry.addShapelessRecipe(new ItemStack(ModArmour.EuroditeLeggingsE, 1), Items.IRON_LEGGINGS, ModItems.tinBag, new ItemStack(Items.DYE, 1, 4), new ItemStack(Items.DYE, 1, 4));
+		 addShapelessRecipe(new ItemStack(ModArmour.EuroditeHelmetE), Items.IRON_HELMET, ModItems.tinBag, Items.POTATO, Items.POTATO, Items.POTATO, Items.POTATO, Items.POTATO);
+		 addShapelessRecipe(new ItemStack(ModArmour.EuroditeChestplateE), Items.IRON_CHESTPLATE, ModItems.tinBag, Items.POTATO);
+		 addShapelessRecipe(new ItemStack(ModArmour.EuroditeLeggingsE), Items.IRON_LEGGINGS, ModItems.tinBag, "dyeBlue", "dyeBlue");
 		 //
-		 GameRegistry.addShapelessRecipe(new ItemStack(ModArmour.EuroditeHelmetF, 1), Items.IRON_HELMET, ModItems.tinBag, Items.POTATO, Items.POTATO, Items.POTATO, Items.POTATO, Items.POTATO, Items.POTATO);
-		 GameRegistry.addShapelessRecipe(new ItemStack(ModArmour.EuroditeChestplateF, 1), Items.IRON_CHESTPLATE, ModItems.tinBag, Items.POTATO, Items.POTATO, Items.POTATO, Items.POTATO, Items.POTATO, Items.POTATO);
-		 GameRegistry.addShapelessRecipe(new ItemStack(ModArmour.EuroditeLeggingsF, 1), Items.IRON_LEGGINGS, ModItems.tinBag, new ItemStack(Items.DYE, 1, 4), new ItemStack(Items.DYE, 1, 4), new ItemStack(Items.DYE, 1, 4));
+		 addShapelessRecipe(new ItemStack(ModArmour.EuroditeHelmetF), Items.IRON_HELMET, ModItems.tinBag, Items.POTATO, Items.POTATO, Items.POTATO, Items.POTATO, Items.POTATO, Items.POTATO);
+		 addShapelessRecipe(new ItemStack(ModArmour.EuroditeChestplateF), Items.IRON_CHESTPLATE, ModItems.tinBag, Items.POTATO, Items.POTATO, Items.POTATO, Items.POTATO, Items.POTATO, Items.POTATO);
+		 addShapelessRecipe(new ItemStack(ModArmour.EuroditeLeggingsF), Items.IRON_LEGGINGS, ModItems.tinBag, "dyeBlue", "dyeBlue", "dyeBlue");
 		 
 		 
 		 
 		 //Green + Paladin
-		 GameRegistry.addShapelessRecipe(new ItemStack(ModArmour.EuroditeHelmetG1, 1), Items.IRON_HELMET, ModItems.tinBag, Items.GHAST_TEAR);
-		 GameRegistry.addShapelessRecipe(new ItemStack(ModArmour.EuroditeChestplateG1, 1), Items.IRON_CHESTPLATE, ModItems.tinBag, Items.GHAST_TEAR);
-		 GameRegistry.addShapelessRecipe(new ItemStack(ModArmour.EuroditeLeggingsG1, 1), Items.IRON_LEGGINGS, ModItems.tinBag, new ItemStack(Items.DYE, 1, 2));
+		 addShapelessRecipe(new ItemStack(ModArmour.EuroditeHelmetG1), Items.IRON_HELMET, ModItems.tinBag, Items.GHAST_TEAR);
+		 addShapelessRecipe(new ItemStack(ModArmour.EuroditeChestplateG1), Items.IRON_CHESTPLATE, ModItems.tinBag, Items.GHAST_TEAR);
+		 addShapelessRecipe(new ItemStack(ModArmour.EuroditeLeggingsG1), Items.IRON_LEGGINGS, ModItems.tinBag, "dyeGreen");
 		 //
-		 GameRegistry.addShapelessRecipe(new ItemStack(ModArmour.EuroditeHelmetG2, 1), Items.IRON_HELMET, ModItems.tinBag, Items.GHAST_TEAR, Items.GOLD_INGOT);
-		 GameRegistry.addShapelessRecipe(new ItemStack(ModArmour.EuroditeChestplateG2, 1), Items.IRON_CHESTPLATE, ModItems.tinBag, Items.GHAST_TEAR, Items.GOLD_INGOT);
-		 GameRegistry.addShapelessRecipe(new ItemStack(ModArmour.EuroditeLeggingsG2, 1), Items.IRON_LEGGINGS, ModItems.tinBag, new ItemStack(Items.DYE, 1, 2), new ItemStack(Items.DYE, 1, 2));
+		 addShapelessRecipe(new ItemStack(ModArmour.EuroditeHelmetG2), Items.IRON_HELMET, ModItems.tinBag, Items.GHAST_TEAR, "ingotGold");
+		 addShapelessRecipe(new ItemStack(ModArmour.EuroditeChestplateG2), Items.IRON_CHESTPLATE, ModItems.tinBag, Items.GHAST_TEAR, "ingotGold");
+		 addShapelessRecipe(new ItemStack(ModArmour.EuroditeLeggingsG2), Items.IRON_LEGGINGS, ModItems.tinBag, "dyeGreen", "dyeGreen");
 		 //
-		 GameRegistry.addShapelessRecipe(new ItemStack(ModArmour.EuroditeHelmetG3, 1), Items.IRON_HELMET, ModItems.tinBag, Items.GHAST_TEAR, Items.GOLD_INGOT, Items.GOLD_INGOT);
-		 GameRegistry.addShapelessRecipe(new ItemStack(ModArmour.EuroditeChestplateG3, 1), Items.IRON_CHESTPLATE, ModItems.tinBag, Items.GHAST_TEAR, Items.GHAST_TEAR, Items.GOLD_INGOT, Items.GOLD_INGOT);
-		 GameRegistry.addShapelessRecipe(new ItemStack(ModArmour.EuroditeLeggingsG3, 1), Items.IRON_LEGGINGS, ModItems.tinBag, new ItemStack(Items.DYE, 1, 2), new ItemStack(Items.DYE, 1, 2), new ItemStack(Items.DYE, 1, 2));
-		 
-		 
-		 
-		 
-		 
+		 addShapelessRecipe(new ItemStack(ModArmour.EuroditeHelmetG3), Items.IRON_HELMET, ModItems.tinBag, Items.GHAST_TEAR, "ingotGold", "ingotGold");
+		 addShapelessRecipe(new ItemStack(ModArmour.EuroditeChestplateG3), Items.IRON_CHESTPLATE, ModItems.tinBag, Items.GHAST_TEAR, Items.GHAST_TEAR, "ingotGold", "ingotGold");
+		 addShapelessRecipe(new ItemStack(ModArmour.EuroditeLeggingsG3), Items.IRON_LEGGINGS, ModItems.tinBag, "dyeGreen", "dyeGreen", "dyeGreen");
 	}
 	 
-	 
+	public static void addRecipe(ItemStack output, Object... args){
+		GameRegistry.addRecipe(new ShapedOreRecipe(output, args));
+	}
+
+	public static void addShapelessRecipe(ItemStack output, Object... args){
+		GameRegistry.addRecipe(new ShapelessOreRecipe(output, args));
+	}
 	
 	public static void registerSmeltingRecipes() {
 		
